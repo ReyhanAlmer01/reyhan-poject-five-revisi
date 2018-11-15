5c05aba67e2d98c2cef82bdb8b2ac69b4a5f4bbb
ReyhanAlmer01 <reyhanalmer99@gmail.com>
2018-11-14 23:11:11 -1200

    five revisi commit

---
+++ src/app/recipeReyhan/recipe-detail-reyhan/recipe-detail-reyhan.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipeReyhan.model';
+import { RecipeReyhanService } from '../recipeReyhan.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+@Component({
+  selector: 'app-recipe-detail-reyhan',
+  templateUrl: './recipe-detail-reyhan.component.html',
+  styleUrls: ['./recipe-detail-reyhan.component.css']
+})
+export class RecipeDetailReyhanComponent implements OnInit {
+  // @Input() 
+  recipeReyhan:Recipe;
+  id:number;
+  constructor(private recipeReyhanService:RecipeReyhanService,
+                      private route:ActivatedRoute,
+                      private router:Router) { }
+
+  ngOnInit() {
+    this.route.params
+    .subscribe(
+      (params:Params)=>{
+        this.id=+params['id'];
+        this.recipeReyhan=this.recipeReyhanService.getRecipe(this.id)
+      }
+    )
+  }
+
+  onAddShoppingList(){
+    this.recipeReyhanService.addIngredientsShoppingList(this.recipeReyhan.ingredient);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo:this.route});
+  }
+}
